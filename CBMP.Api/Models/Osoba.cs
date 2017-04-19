2269fb525fc59098461d5a3f3d6e5a1e7a6e53cf
ptk86 <piotr.kilinski@gmail.com>
2017-04-19 23:34:47 +0200

    OsobaUpdate Migration

--- CBMP.Api/Models/Osoba.cs
+++ CBMP.Api/Models/Osoba.cs
@@ -14,8 +14,10 @@ namespace CBMP.Api.Models
         public string Pesel { get; set; }
         public Imie Imie { get; set; }
         public string Nazwisko { get; set; }
+        public int IdMiejsceUrodzenia { get; set; }
         public Miejscowosc MiejsceUrodzenia { get; set; }
         public string KodPocztowy { get; set; }
+        public int IdMiejscowosc { get; set; }
         public Miejscowosc Miejscowosc { get; set; }
         public string Ulica { get; set; }
         public string NumerDomu { get; set; }
