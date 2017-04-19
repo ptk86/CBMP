49b1f6b40c89d89025ff855d16d948d788189212
ptk86 <piotr.kilinski@gmail.com>
2017-04-19 23:11:39 +0200

    Dodano Kolekcje Osob do Imie i Miejscowosc

--- CBMP.Api/Models/Imie.cs
+++ CBMP.Api/Models/Imie.cs
@@ -1,8 +1,19 @@
-﻿namespace CBMP.Api.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CBMP.Api.Models
 {
     public class Imie
     {
         public int Id { get; set; }
         public string Nazwa { get; set; }
+        public ICollection<Osoba> Osoby { get; set; }
+
+        public Imie()
+        {
+            Osoby = new Collection<Osoba>();
+        }
+
     }
 }
--- CBMP.Api/Models/Miejscowosc.cs
+++ CBMP.Api/Models/Miejscowosc.cs
@@ -1,8 +1,17 @@
-﻿namespace CBMP.Api.Models
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CBMP.Api.Models
 {
     public class Miejscowosc
     {
         public int Id { get; set; }
         public string Nazwa { get; set; }
+        public ICollection<Osoba> Osoby { get; set; }
+
+        public Miejscowosc()
+        {
+            Osoby = new Collection<Osoba>();
+        }
     }
 }
