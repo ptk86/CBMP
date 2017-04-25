6ac5e27c71fd5163da1f20479c39b52645a88d7e
ptk86 <piotr.kilinski@gmail.com>
2017-04-26 00:01:31 +0200

    Added EdytujOsobe

---
+++ CBMP.Client/src/app/osoba/edytuj-osobe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cbmp-edytuj-osobe',
+  templateUrl: './edytuj-osobe.component.html',
+  styles: []
+})
+export class EdytujOsobeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
