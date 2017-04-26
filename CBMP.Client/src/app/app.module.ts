c74dea406b626e725835204b26961905f95484fe
ptk86 <piotr.kilinski@gmail.com>
2017-04-26 02:12:28 +0200

    Updated OsobyController so it supports multiple get methods

--- CBMP.Client/src/app/app.module.ts
+++ CBMP.Client/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AutoCompleteModule } from 'primeng/components/autocomplete/autocomplete';
+
 
 import { AppComponent } from './app.component';
 import { EdytujOsobeComponent } from './osoba/edytuj-osobe.component';
@@ -13,8 +15,9 @@ import { EdytujOsobeComponent } from './osoba/edytuj-osobe.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
